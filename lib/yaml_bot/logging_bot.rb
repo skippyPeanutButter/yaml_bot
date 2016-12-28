2d1d656377d5985be3a430fd73ddf8dc7878e716
Luis Ortiz <lortiz1145@gmail.com>
2016-12-28 03:17:40 -0500

    Convert Logging module to a class
    - Add log_file variable to Logging class

--- lib/yaml_bot/logging_bot.rb
+++ lib/yaml_bot/logging_bot.rb
@@ -1,19 +1,24 @@
 module YamlBot
-  module Logging
+  class LoggingBot
     ESCAPES = { green: "\033[32m",
                 yellow: "\033[33m",
                 red: "\033[31m",
                 reset: "\033[0m" }.freeze
 
+    @@log_file = File.new('yaml_bot.log', 'w')
+
     def self.info(message)
+      @@log_file.write(message + "\n")
       emit(message: message, color: :green)
     end
 
     def self.warn(message)
+      @@log_file.write(message + "\n")
       emit(message: message, color: :yellow)
     end
 
     def self.error(message)
+      @@log_file.write(message + "\n")
       emit(message: message, color: :red)
     end
 
@@ -24,6 +29,10 @@ module YamlBot
       print message
       print ESCAPES[:reset]
       print "\n"
+    end
+
+    def self.close_log
+      @@log_file.close
     end
   end
 end
--- lib/yaml_bot/rules_bot.rb
+++ lib/yaml_bot/rules_bot.rb
@@ -1,5 +1,5 @@
 require 'yaml'
-require 'yaml_bot/logging'
+require 'yaml_bot/logging_bot'
 require 'yaml_bot/validation_error'
 
 module YamlBot
@@ -16,7 +16,7 @@ module YamlBot
       fail_if_rules_are_not_loaded
       validate_root_keys
       check_top_level_keys
-      YamlBot::Logging.info 'Rules file validated.'
+      YamlBot::LoggingBot.info 'Rules file validated.'
     end
 
     private
@@ -61,10 +61,10 @@ module YamlBot
     def log_missing_key_type(key_type)
       if key_type == :required
         @missing_required_keys = true
-        YamlBot::Logging.info 'No required keys specified.'
+        YamlBot::LoggingBot.info 'No required keys specified.'
       else
         @missing_optional_keys = true
-        YamlBot::Logging.info 'No optional keys specified.'
+        YamlBot::LoggingBot.info 'No optional keys specified.'
       end
     end
 
